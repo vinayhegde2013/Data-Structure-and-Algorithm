31fce6fd46fddc11cb7594889e1dda3ab2c19c69
Vinay Hegde <45894236+vinayhegde2013@users.noreply.github.com>
2020-06-30 18:39:06 +0530

    added dp programme

---
+++ Dynamic-Programming/RobotUniquePath.java
@@ -0,0 +1,26 @@
+package com.vinay.hegde.algorithm;
+
+/**
+ * Problem Statement : A robot is located at the top-left corner of a m x n grid
+ * (marked 'Start' in the diagram below). The robot can only move either down or
+ * right at any point in time. The robot is trying to reach the bottom-right
+ * corner of the grid. How many possible unique paths are there?
+ */
+public class RobotUniquePath {
+
+	public static void main(String[] args) {
+		System.out.println(uniquePaths(3, 2));
+	}
+
+	public static int uniquePaths(int m, int n) {
+		int[][] dp = new int[m][n];
+		for (int i = 0; i < n; i++)
+			dp[0][i] = 1;
+		for (int i = 0; i < m; i++)
+			dp[i][0] = 1;
+		for (int i = 1; i < m; i++)
+			for (int j = 1; j < n; j++)
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+		return dp[m - 1][n - 1];
+	}
+}
