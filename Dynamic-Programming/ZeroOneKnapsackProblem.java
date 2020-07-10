a1d6155ddea57ee58414eb956e06718b598a9d9e
Vinay Hegde <45894236+vinayhegde2013@users.noreply.github.com>
2020-07-11 00:12:20 +0530

    updating knapsack algo

--- Dynamic-Programming/ZeroOneKnapsackProblem.java
+++ Dynamic-Programming/ZeroOneKnapsackProblem.java
@@ -8,26 +8,24 @@ public class ZeroOneKnapsackProblem {
 		int capacity = 30;
 		int weight[] = { 10, 20, 1, 9, 90 };
 		int profit[] = { 2, 3, 99, 21, 8 };
-		System.out.println(calculateKnapSack(n, capacity, weight, profit));
+		System.out.println(calculateKnapSack(n, capacity, profit, weight));
 
 	}
 
-	private static int calculateKnapSack(int n, int capacity, int[] weight, int[] profit) {
-
-		int dp[][] = new int[n + 1][capacity + 1];
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= capacity; j++) {
-				if (i == 0 || j == 0)
-					dp[i][j] = 0;
-				else if (weight[i - 1] <= capacity)
+	 private static int calculateKnapSack(int n,int c, int[] value, int[] weight){
+        int dp[][]=new int[n+1][c+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=c;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0; 
+                else if(weight[i-1]<=j)
 					// first case : include item ( currentProfit + remaining profit )
 					// Second case : exclude item ( previous profit)
-					dp[i][capacity] = Math.max(profit[i - 1] + dp[i - 1][capacity - weight[i - 1]],
-							dp[i - 1][capacity]);
-				else
-					dp[i][capacity] = dp[i - 1][capacity];
-			}
-		}
-		return dp[n][capacity];
-	}
+                    dp[i][j]=Math.max( value[i-1] +dp[i-1][j-weight[i-1]], dp[i-1][j] );
+                else
+                    dp[i][j]=dp[i-1][j];           
+            }
+        }
+        return dp[n][c];
+    }
 }
