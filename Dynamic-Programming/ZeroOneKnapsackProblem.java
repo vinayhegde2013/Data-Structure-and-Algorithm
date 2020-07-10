f537db643bad5670e2ca7a6f03f3f519ca628c6d
Vinay Hegde <45894236+vinayhegde2013@users.noreply.github.com>
2020-07-11 00:15:01 +0530

    update knapsack algo

--- Dynamic-Programming/ZeroOneKnapsackProblem.java
+++ Dynamic-Programming/ZeroOneKnapsackProblem.java
@@ -12,20 +12,20 @@ public class ZeroOneKnapsackProblem {
 
 	}
 
-	 private static int calculateKnapSack(int n,int c, int[] value, int[] weight){
-        int dp[][]=new int[n+1][c+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=c;j++){
-                if(i==0 || j==0)
-                    dp[i][j]=0; 
-                else if(weight[i-1]<=j)
+	private static int calculateKnapSack(int n, int c, int[] value, int[] weight) {
+		int dp[][] = new int[n + 1][c + 1];
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= c; j++) {
+				if (i == 0 || j == 0)
+					dp[i][j] = 0;
+				else if (weight[i - 1] <= j)
 					// first case : include item ( currentProfit + remaining profit )
 					// Second case : exclude item ( previous profit)
-                    dp[i][j]=Math.max( value[i-1] +dp[i-1][j-weight[i-1]], dp[i-1][j] );
-                else
-                    dp[i][j]=dp[i-1][j];           
-            }
-        }
-        return dp[n][c];
-    }
+					dp[i][j] = Math.max(value[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
+				else
+					dp[i][j] = dp[i - 1][j];
+			}
+		}
+		return dp[n][c];
+	}
 }
