2c7afbb75bc950ec77496c286eef485cc7ad6478
Daemon <45894236+vinayhegde2013@users.noreply.github.com>
2020-07-24 22:21:53 +0530

    Update KDistanceTree.java

--- Trees/KDistanceTree.java
+++ Trees/KDistanceTree.java
@@ -5,9 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-//We are given a binary tree (with root node root), a target node, and an integer value K.
-//Return a list of the values of all nodes that have a distance K from the target node.  
-//The answer can be returned in any order.
+//Return a list of the values of all nodes that have a distance K from the target node
 
 public class KDistanceTree {
 
