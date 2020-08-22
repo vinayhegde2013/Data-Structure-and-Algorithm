d2583ffdc3f29ba37c4281fd4679d0c31fc34223
Daemon <45894236+vinayhegde2013@users.noreply.github.com>
2020-08-22 15:31:54 +0530

    Create AllSubSequence.java

---
+++ General/AllSubSequence.java
@@ -0,0 +1,27 @@
+package com.comp.prog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllSubSequence {
+
+	static List<String> result = new ArrayList<>();
+
+	public static void main(String[] args) {
+
+		String str = "ab";
+		printAllSubSequence(str, "");
+		System.out.println(result);
+	}
+
+	private static void printAllSubSequence(String str, String ans) {
+
+		if (str.length() == 0) {
+			result.add(ans);
+			return;
+		}
+		printAllSubSequence(str.substring(1), ans + str.charAt(0));
+		printAllSubSequence(str.substring(1), ans);
+	}
+
+}
