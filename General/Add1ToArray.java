56e897c928a5cad27205cdc6b173d083ed13d9bd
Vinay Hegde <45894236+vinayhegde2013@users.noreply.github.com>
2021-03-27 19:06:06 +0530

    Update Add1ToArray.java

--- General/Add1ToArray.java
+++ General/Add1ToArray.java
@@ -4,7 +4,7 @@ public class Add1ToArray {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 9, 9, 9, 9 };
+		int[] arr = { 2,3,5,6 };
 		recursive(arr, arr.length - 1);
 		System.out.println(arr[0]);
 		for (int i : arr)
