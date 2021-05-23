07187e66256440689b2961c3eea594af1ed57ada
Vinay Hegde <45894236+vinayhegde2013@users.noreply.github.com>
2021-05-23 19:38:22 +0530

    Update LRUCache.java

--- Design/LRUCache.java
+++ Design/LRUCache.java
@@ -1,1 +1,55 @@
 
+
+package com.comp.prog;
+
+import java.util.Deque;
+import java.util.HashMap;
+
+/**
+ * @author Vinay Hegde
+ *
+ */
+public class LRUCache {
+
+	private static final int CAPACITY = 100;
+	static Deque<Integer> dq;
+	static HashMap<Integer, Integer> map = new HashMap<>();
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		put(1, 3);
+		get(1);
+		put(2, 4);
+		put(5, 2);
+		get(2);
+	}
+
+	public static int get(int key) {
+		if (!map.containsKey(key))
+			return -1;
+		else {
+			dq.remove(key);
+			dq.addFirst(key);
+			return map.get(key);
+		}
+	}
+
+	public static void put(int key, int value) {
+		if (map.containsKey(key)) {
+			dq.remove(key);
+			dq.addFirst(key);
+		} else {
+			if (dq.size() == CAPACITY) {
+				int lkey = dq.removeLast();
+				map.remove(lkey);
+			}
+			dq.addFirst(key);
+		}
+
+		map.put(key, value);
+	}
+
+}
