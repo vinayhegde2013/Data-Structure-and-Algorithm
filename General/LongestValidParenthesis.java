e41513064fe9fc427438d44eb8c25a3106d4b3f6
Vinay Hegde <45894236+vinayhegde2013@users.noreply.github.com>
2021-06-12 14:36:16 +0530

    Create LongestValidParenthesis.java

---
+++ General/LongestValidParenthesis.java
@@ -0,0 +1,37 @@
+package com.comp.prog;
+
+import java.util.Stack;
+
+public class MergeSortedList {
+
+      public static void main(String[] args) {
+ 
+  	      System.out.println(longestValidParentheses("((((()))))"));
+  }
+  
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (!stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+    
+    private static int longestValidParentheses(String s) {
+           int maxlen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 2; j <= s.length(); j+=2) {
+                if (isValid(s.substring(i, j))) {
+                    maxlen = Math.max(maxlen, j - i);
+                }
+            }
+        }
+        return maxlen;
+    }
+}
